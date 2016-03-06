1dd11a5fc7428bc6433a87f79c6cb15548b4868a
Braeden Soetaert <soetaertbs@hotmail.com>
2016-03-06 14:38:06 -0700

    test android studio commit

--- app/src/main/java/com/example/bbschool/bbremote/ConnectionActivity.java
+++ app/src/main/java/com/example/bbschool/bbremote/ConnectionActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class ConnectionActivity extends AppCompatActivity {
-
+//test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
