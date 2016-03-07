ba49339c346d3fc0649dc2822f1ca41b81b2683d
Braeden Soetaert <soetaertbs@hotmail.com>
2016-03-06 22:17:04 -0700

    Initial classes again

---
+++ BBRemoteMobile/app/src/main/java/com/example/bbschool/bbremotemobile/KeyboardFragment.java
@@ -0,0 +1,22 @@
+package com.example.bbschool.bbremotemobile;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class KeyboardFragment extends Fragment {
+
+    public KeyboardFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_keyboard, container, false);
+    }
+}
